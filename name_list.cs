95e53c1b174284bcb73e6785a48f7221ebb7a53e
hayato0206 <suzu88haya10@gmail.com>
2021-05-31 18:49:42 +0900

    suzuki_henkou

--- name_list.cs
+++ name_list.cs
@@ -7,11 +7,13 @@ public class name_list : MonoBehaviour
     int MAXMENBER = 10;
     string[] menbers = new string[10];
     string men1 = "kaneko"; 
+    string men2 = "suzuki";
 
     // Start is called before the first frame update
     void Start()
     {
         menbers[0] = men1;
+        menbers[1] = men2;
 
     }
 
