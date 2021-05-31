ef44f8c598c4825df110f2908d951f1867842b1e
b20p010 <b20p010@akita-pu.ac.jp>
2021-05-31 18:25:24 +0900

    test

---
+++ name_list.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class name_list : MonoBehaviour
+{
+    string[] menbers = new string[10];
+    string men1 = "kaneko"; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        menbers[0] = men1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
