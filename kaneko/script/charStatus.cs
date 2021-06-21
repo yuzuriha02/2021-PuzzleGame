7caf9fe64cf45248b8a5c5fcd5827e6c44ebca53
b20p010 <b20p010@akita-pu.ac.jp>
2021-06-21 21:04:38 +0900

    6/21_バフアイコン表示

--- kaneko/script/charStatus.cs
+++ kaneko/script/charStatus.cs
@@ -8,24 +8,41 @@ public class charStatus : MonoBehaviour
     public int hp;
     public int mp;
     public int atackPower;//たたかうコマンドの攻撃力
-    [HideInInspector]public bool defenceSwich=false;
+    [HideInInspector]public bool defenceSwich=false;//防御状態フラグ
+    [SerializeField]GameObject Abuff;
+    [SerializeField]GameObject Adebuff;
+    [SerializeField]GameObject Dbuff;
+    [SerializeField]GameObject Ddebuff;
+    
     // Start is called before the first frame update
     void Start()
     {
+        /*
+        Abuff = GameObject.Find("attack_buff");
+        Adebuff = GameObject.Find("attack_debuff");
+        Dbuff = GameObject.Find("deffence_buff");
+        Ddebuff = GameObject.Find("deffence_debuff");
+        */
+        Abuff.SetActive(false);
+        Adebuff.SetActive(false);
+        Dbuff.SetActive(false);
+        Ddebuff.SetActive(false);
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     public void defence(bool d){
         if(d){
+            Dbuff.SetActive(true);
             defenceSwich = true;
         }
-        if(d){
+        else{
             defenceSwich = false;
+            Dbuff.SetActive(false);
         }
 
     }
